bc1407b39a0acfe9433c9b250b80daf8696e2b1e
MrCoopa <bluestar0013@gmail.com>
2026-06-11 09:51:28 +0200

    design: adjust battery indicator padding to ensure bar is perfectly centered and doesn't touch borders

--- client/src/components/BatteryIndicator.jsx
+++ client/src/components/BatteryIndicator.jsx
@@ -13,9 +13,9 @@ const BatteryIndicator = ({ percentage, className = "" }) => {
 
     return (
         <div className={`flex items-center ${className}`}>
-            <div className="relative w-6 h-3 border border-gray-400 rounded-md p-[1.5px] flex items-center bg-gray-50/50">
+            <div className="relative w-6 h-3.5 border border-gray-400 rounded-md p-[2px] flex items-center bg-gray-50/50">
                 <div
-                    className={`h-full rounded-[3px] bg-gradient-to-r transition-all duration-500 ${gradientClass}`}
+                    className={`h-full rounded-[2px] bg-gradient-to-r transition-all duration-500 ${gradientClass}`}
                     style={{ width: `${fillLevel}%` }}
                 />
                 {/* Battery terminal tip */}
